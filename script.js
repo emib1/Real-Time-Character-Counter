f86fe7f0be3e508a0cb5edab688a4370ba80677b
emib1 <ibeman851@gmail.com>
2022-09-18 15:14:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const textArea = document.getElementById('textarea')
+const totalCounter = document.getElementById('total-counter')
+const totalRemaining = document.getElementById('remaining-counter')
+
+textArea.addEventListener("keyup", () => {
+    updateCounter()
+
+})
+
+updateCounter()
+
+
+function updateCounter() {
+    totalCounter.innerText = textArea.value.length
+    totalRemaining.innerText = textArea.getAttribute("maxLength") - textArea.value.length
+
+}
